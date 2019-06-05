c470644a8448b68f007ce10bf29dd79e3666cc22
mrkchoi <kennethichoi@gmail.com>
2019-06-05 14:45:51 -0700

    Create user migration + model & validations

---
+++ NineNineCats_auth/app/models/user.rb
@@ -0,0 +1,45 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+  attr_reader :password
+
+  # after_initialize :ensure_session_token
+
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, :session_token, uniqueness: true
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil if user.nil?
+    is_password?(password) ? user : nil
+  end
+
+  # def ensure_session_token
+
+  # end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def password=(pw)
+    @password = pw
+    self.password_digest = BCrypt::Password.create(pw)
+  end
+
+  def is_password?(pw)
+    BCrypt::Password.new(self.password_digest).is_password?(pw)
+  end
+end
